e0e006de9d67762fdb1a5a242d7b8ca405018156
Yijie Liu <2707167603@qq.com>
2026-04-01 08:54:32 +0800

    fix: correct signal handling error log message

--- webserver.cpp
+++ webserver.cpp
@@ -410,7 +410,7 @@ void WebServer::eventLoop()
             {
                 bool flag = dealwithsignal(timeout, stop_server);
                 if (false == flag)
-                    LOG_ERROR("%s", "dealclientdata failure");
+                    LOG_ERROR("%s", "dealwithsignal failure");
             }
             //处理客户连接上接收到的数据
             else if (events[i].events & EPOLLIN)
